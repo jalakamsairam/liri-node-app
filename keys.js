75a2e624d4411ebac3f45bd051f658e5e3267cd5
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2017-10-16 19:52:20 -0700

    initial commit with all dependencies installed

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+	 consumer_key:'ikV5fYuiB2MlE1Q1XJUd6Utfj' ,
+	 consumer_secret: 'vLPHthSAKpaMK642PA8MYMf5LB6EBgspVWQwxiC9T8bZtqwxxC',
+	 access_token_key: '162722504-tADid7IBjh6hQp0KfapBri6cuKIDPsbNZxTIjrnT',
+	 access_token_secret: 'Dz4yYelcw8Kzgq1AzheFHR5AAMkgBOAVkPpRNrydM5CEL',
+}
+
+module.exports = twitterKeys;
